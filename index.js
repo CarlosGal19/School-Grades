002ef0929f57e521dc36aa4d84a19789993eca39
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-17 09:05:22 -0600

    fix: call to function that connects db

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import studentRoutes from "./routes/studentRoutes.js";
+import connectDB from "./config/db.js";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+connectDB();
 
 app.use("/students", studentRoutes);
 
