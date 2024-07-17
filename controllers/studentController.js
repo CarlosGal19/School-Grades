6532980b62542b872182e4f51c8235a8e4a668e8
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-17 09:06:45 -0600

    feat: create function that gets one and all students

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,7 +1,21 @@
 import Student from "../models/student.model.js";
 
 const getStudent = async (req, res) => {
-
+    try {
+        const student = await Student.findById(req.params.id);
+        res.status(200).json(student);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export { getStudent };
+const getStudents = async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.status(200).json(students);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export { getStudent, getStudents };
